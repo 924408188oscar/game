aec5389419bb52f17910b13dfed4e6595790ef6b
liruoliang <liruoliang@sigmayun.com>
2020-11-17 17:10:02 +0800

    修改temp文件

--- src/main/java/cn/oscar/common/Temp.java
+++ src/main/java/cn/oscar/common/Temp.java
@@ -2,7 +2,7 @@ package cn.oscar.common;
 
 public class Temp {
     public static void main(String[] args) {
-        String chinse= "中人工";
+        String chinse= "中在人工111";
 
         System.out.println(chinse);
 
